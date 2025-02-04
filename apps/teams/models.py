6fb1ab0ef6f9b1f816688437a7f503cace10ed84
Jung <jjlee030415@gmail.com>
2025-02-04 16:45:20 +0900

    💡 Model 만들때 상속받는 모델을 BaseModel로 수정

--- apps/teams/models.py
+++ apps/teams/models.py
@@ -1,8 +1,11 @@
 from django.db import models
 
+from apps.common.models import BaseModel
+from apps.users.models import User
+
 
 # 팀 정보를 저장하는 모델
-class Team(models.Model):
+class Team(BaseModel):
     # 팀 이름(최대 30글자, 중복불가)
     name = models.CharField(max_length=30, unique=True, help_text="팀 이름")
     # 소셜 미디어 URL 정보를 JSON 형식으로 저장 (insta, facebook, youtube, twitter)
@@ -17,7 +20,7 @@ class Team(models.Model):
 
 
 # 팀 일정을 저장하는 모델
-class TeamSchedule(models.Model):
+class TeamSchedule(BaseModel):
     # 해당 일정이 속한 팀 (Team 모델과의 외래키 관계)
     team_id = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="schedules", help_text="팀 식별자")
     # 일정 카테고리 (경기, 연습 등)
@@ -40,11 +43,11 @@ class TeamSchedule(models.Model):
 
 
 # 팀 구독 정보를 저장하는 모델
-class TeamSub(models.Model):
+class TeamSub(BaseModel):
     # 구독한 유저 식별자(한명의 유저는 하나의 팀만 구독이 가능함)
-    user_id = models.OneToOneField("User", on_delete=models.CASCADE, help_text="구독한 유저 식별자")
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE, help_text="구독한 유저 식별자")
     # 구독한 팀 식별자(하나의 팀은 여러명의 구독자를 가질 수 있음)
-    team_id = models.ForeignKey("Team", on_delete=models.CASCADE, help_text="구독한 팀 식별자")
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE, help_text="구독한 팀 식별자")
     # 구독 활성화 상태 여부 (True면 활성화, False면 비활성화)
     is_active = models.BooleanField(default=True, help_text="구독 활성화 상태 여부")
 
@@ -57,7 +60,7 @@ class TeamSub(models.Model):
 
 
 # 팀 이미지 정보를 저장하는 모멜
-class TeamImage(models.Model):
+class TeamImage(BaseModel):
     # 팀에 해당하는 이미지
     team_id = models.ForeignKey(Team, on_delete=models.CASCADE)
     # 이미지 카테고리(프로필, 배경, 갤러리)
