9595d8c6c2e0ec6d51bea3840bfbd91aca8f743b
Jung <jjlee030415@gmail.com>
2025-02-04 10:59:42 +0900

    ✨팀 일정을 저장하는 모델 추가

--- apps/teams/models.py
+++ apps/teams/models.py
@@ -11,3 +11,23 @@ class Team(models.Model):
     )
     # 팀이 활성화 상태 여부(True면 활성화 상태 False면 비활성화)
     is_active = models.BooleanField(default=True, help_text="팀 활성화 상태 여부")
+
+
+# 팀 일정을 저장하는 모델
+class TeamSchedule(models.Model):
+    # 해당 일정이 속한 팀 (Team 모델과의 외래키 관계)
+    team_id = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="schedules", help_text="팀 식별자")
+    # 일정 카테고리 (경기, 연습 등)
+    category = models.CharField(max_length=10, help_text="일정 카테고리")
+    # 이벤트 시작 시간 (종료 시간보다 이전이어야 함)
+    start_date = models.DateTimeField(help_text="이벤트 시작 (end_date보다 작거나 같아야 함)")
+    # 이벤트 종료 시간 (시작 시간보다 이후이어야 함)
+    end_date = models.DateTimeField(help_text="이벤트 종료 (start_date보다 크거나 같아야 함)")
+    # 일정 장소
+    place = models.CharField(max_length=30, help_text="장소")
+    # 일정 제목
+    title = models.CharField(max_length=50, help_text="제목")
+    # 일정에 대한 상세 내용
+    detail = models.CharField(max_length=255, blank=True, null=True, help_text="내용")
+    # 일정 활성화 상태 여부 (True면 활성화, False면 비활성화)
+    is_active = models.BooleanField(default=True, help_text="일정 활성화 여부")
