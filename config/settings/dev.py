84beb5fd17912e5ab8812371b7425fa4d72bbaf0
hyo <umdoongs@gmail.com>
2025-02-04 15:01:45 +0900

    :sparkles: dev, prod 환경 설정 기능 추가
    # Conflicts:
    #	config/settings/dev.py

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -23,6 +23,7 @@ INSTALLED_APPS += [
     "rest_framework_simplejwt.token_blacklist",
     "apps.users.apps.UsersConfig",
     "apps.players.apps.PlayersConfig",
+    "apps.users.apps.UsersConfig"
 ]
 
 DATABASES = {
