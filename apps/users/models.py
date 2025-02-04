ecb1226413e8734c029d88943ee760855eee1a0e
Jung <jjlee030415@gmail.com>
2025-02-04 16:17:05 +0900

    ✨ User 이미지, 사용자 약관동의 및 저장하는 모델 추가

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from apps.common.models import BaseModel
 
 
+# 사용자 정보를 저장하는 모델
 class User(BaseModel, AbstractBaseUser):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=130)
@@ -14,3 +15,24 @@ class User(BaseModel, AbstractBaseUser):
 
     # 로그인 시 username이 아니라 email로 로그인하게 됨(식별자가 email)
     USERNAME_FIELD = "email"
+
+
+# 유저 이미지 정보를 저장하는 모멜
+class UserImage(BaseModel):
+    user_id = models.OneToOneField(User, on_delete=models.CASCADE, related_name="images")
+    url = models.CharField(max_length=255)
+
+
+# 사용자의 약관 동의 정보를 저장하는 모델
+class TermsAgreements(BaseModel):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    terms_id = models.ForeignKey("Terms", on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+
+
+# 약관 정보를 저장하는 모델
+class Terms(BaseModel):
+    name = models.CharField(max_length=50)
+    detail = models.TextField()
+    is_active = models.BooleanField(default=True)
+    is_required = models.BooleanField(default=True)
