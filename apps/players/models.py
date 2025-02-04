5932953aa31b72ff3903f12c1ad08210869b0a4a
BlueLight <dlrkdmf10@gmail.com>
2025-02-04 13:33:35 +0900

    :sparkles: player schedule 관리 모델 작성

--- apps/players/models.py
+++ apps/players/models.py
@@ -22,7 +22,9 @@ class Player(BaseModel):
     gamename = models.CharField(max_length=50) # 게임 닉네임
     position = models.CharField(
         max_length=10,
-        choices=[(position.value, position.name) for position in Position],
+        choices=[
+            (position.value, position.name) for position in Position
+        ],
         null=False, # DB에서 NULL을 허용하지 않음
         blank=False, # 폼에서 빈 값 입력을 허용하지 않음
         default=None # 기본값을 None 으로 설정하여 선택 강제
@@ -39,3 +41,22 @@ class PlayerSub(BaseModel):
     player = models.ForeignKey(Player, on_delete=models.CASCADE) # 특정 선수
     user = models.ForeignKey(User, on_delete=models.CASCADE) # 특정 선수를 구독 한 유저
     is_active = models.BooleanField(default=True) # 구독 활성화 여부
+
+
+# 선수 스케줄 관리 모델
+class PlayerSchedule(BaseModel):
+    player = models.ForeignKey(Player, on_delete=models.CASCADE)
+    category = models.CharField(
+        max_length=10,
+        choices=[
+            ("생일", "생일"),
+            ("경기", "경기일정"),
+            ("개인방송", "개인방송")
+        ]
+    ) # 스케줄 분류 항목
+    start_date = models.DateTimeField() # 이벤트 시작 일시
+    end_date = models.DateTimeField() # 이벤트 종료 일시
+    place = models.CharField(max_length=30, null=True)
+    title = models.CharField(max_length=50, null=False, blank=False, default=None)
+    detail = models.CharField(max_length=255, blank=True) # 이벤트 상세 내용
+    is_active = models.BooleanField(default=True) # 스케줄 활성화 여부
