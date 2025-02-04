11e536681b59dec6c2b73b5e9a1a92d2540e13b8
Jung <jjlee030415@gmail.com>
2025-02-04 18:58:19 +0900

    💡 SoftDeleteModel를 상속 받아사용, # type: ignore추가

--- apps/teams/models.py
+++ apps/teams/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django_softdelete.models import SoftDeleteModel  # type: ignore
 
 from apps.common.models import BaseModel
 from apps.users.models import User
 
 
 # 팀 정보를 저장하는 모델
-class Team(BaseModel):
+class Team(BaseModel, SoftDeleteModel):  # type: ignore
     # 팀 이름(최대 30글자, 중복불가)
     name = models.CharField(max_length=30, unique=True, help_text="팀 이름")
     # 소셜 미디어 URL 정보를 JSON 형식으로 저장 (insta, facebook, youtube, twitter)
@@ -18,7 +19,7 @@ class Team(BaseModel):
 
 
 # 팀 일정을 저장하는 모델
-class TeamSchedule(BaseModel):
+class TeamSchedule(BaseModel, SoftDeleteModel):  # type: ignore
     # 해당 일정이 속한 팀 (Team 모델과의 외래키 관계)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="schedules", help_text="팀 식별자")
     # 일정 카테고리 (경기, 연습 등)
@@ -39,7 +40,7 @@ class TeamSchedule(BaseModel):
 
 
 # 팀 구독 정보를 저장하는 모델
-class TeamSub(BaseModel):
+class TeamSub(BaseModel, SoftDeleteModel):  # type: ignore
     # 구독한 유저 식별자(한명의 유저는 하나의 팀만 구독이 가능함)
     user = models.OneToOneField(User, on_delete=models.CASCADE, help_text="구독한 유저 식별자")
     # 구독한 팀 식별자(하나의 팀은 여러명의 구독자를 가질 수 있음)
@@ -54,7 +55,7 @@ class TeamSub(BaseModel):
 
 
 # 팀 이미지 정보를 저장하는 모멜
-class TeamImage(BaseModel):
+class TeamImage(BaseModel, SoftDeleteModel):  # type: ignore
     # 팀에 해당하는 이미지
     team = models.ForeignKey(Team, on_delete=models.CASCADE)
     # 이미지 카테고리(프로필, 배경, 갤러리)
@@ -67,13 +68,13 @@ class TeamImage(BaseModel):
 
 
 # 팀 관련 태그 정보를 저장하는 모델
-class TeamTags(BaseModel):
+class TeamTags(BaseModel, SoftDeleteModel):  # type: ignore
     # 태그명
     name = models.CharField(max_length=50, unique=True, help_text="관련 검색어")
 
 
 # 팀과 태그 간의 N:M 관계를 저장하는 모델 (팀에게 태그 등록)
-class TeamTagged(BaseModel):
+class TeamTagged(BaseModel, SoftDeleteModel):  # type: ignore
     # 태그가 등록된 팀 (Team 모델과의 외래키 관계)
     team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="tagged_tags", help_text="팀 식별자")
     # 등록된 태그 (TeamTags 모델과의 외래키 관계)
