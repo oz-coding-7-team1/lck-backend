dabdfadafee4aed5c7b6fc2cd02d5f32451a0c4b
hyo <umdoongs@gmail.com>
2025-02-04 11:44:09 +0900

    :pencil2: 오타수정

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -15,8 +15,8 @@ INSTALLED_APPS += [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": "oz_main",  # 로컬 개발용 DB 이름
-        "USER": "lck",  # PostgreSQL 사용자명
+        "NAME": "oz_main_dev",  # 로컬 개발용 DB 이름
+        "USER": "oz_main_dev",  # PostgreSQL 사용자명
         "PASSWORD": "1234",  # 비밀번호
         "HOST": "localhost",
         "PORT": "5432",
