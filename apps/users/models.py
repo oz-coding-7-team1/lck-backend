b15ad2768ae4aef087b7b80e0a62ae1afd4446ab
hyo <umdoongs@gmail.com>
2025-02-04 17:17:04 +0900

    :sparkles: user nickname unique추가, settings.base에 AUTH_USER_MODEL을 custom user로 변경 하는 코드 추가

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,11 +1,14 @@
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from typing import Optional, Union
+
+from django.contrib.auth.models import AbstractBaseUser, AnonymousUser, PermissionsMixin
 from django.db import models
-from django_softdelete.models import SoftDeleteModel
+from django.db.models.base import Model
+from django_softdelete.models import SoftDeleteModel  # type: ignore
 
 from apps.common.models import BaseModel
 
 
-class User(BaseModel, AbstractBaseUser, PermissionsMixin, SoftDeleteModel):
+class User(BaseModel, AbstractBaseUser, PermissionsMixin, SoftDeleteModel):  # type: ignore
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=130)
     nickname = models.CharField(max_length=20, unique=True)
@@ -16,13 +19,13 @@ class User(BaseModel, AbstractBaseUser, PermissionsMixin, SoftDeleteModel):
     # 로그인 시 username이 아니라 email로 로그인하게 됨(식별자가 email)
     USERNAME_FIELD = "email"
 
-    def has_perm(self, perm, obj=None) -> bool:
+    def has_perm(self, perm: str, obj: Optional[Union[Model, AnonymousUser]] = None) -> bool:
         # 사용자가 superuser인 경우 Django의 모든 권한 부여
         if self.is_superuser:
             return True
         return False
 
-    def has_module_perms(self, app_label) -> bool:
+    def has_module_perms(self, app_label: str) -> bool:
         # 사용자가 superuser인 경우 모든 앱의 권한을 부여
         if self.is_superuser:
             return True
