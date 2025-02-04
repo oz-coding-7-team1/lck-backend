59c290dc7c424ba86054a14f8378a5a11bb64d7e
hyo <umdoongs@gmail.com>
2025-02-04 16:59:47 +0900

    :sparkles: user nickname unique추가, settings.base에 AUTH_USER_MODEL을 custom user로 변경 하는 코드 추가

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,14 +1,14 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from django_softdelete.models import SoftDeleteModel
 
 from apps.common.models import BaseModel
 
 
-# 사용자 정보를 저장하는 모델
-class User(BaseModel, AbstractBaseUser, PermissionsMixin):
+class User(BaseModel, AbstractBaseUser, PermissionsMixin, SoftDeleteModel):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=130)
-    nickname = models.CharField(max_length=20)
+    nickname = models.CharField(max_length=20, unique=True)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
     is_superuser = models.BooleanField(default=False)
