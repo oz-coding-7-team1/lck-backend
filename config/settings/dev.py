ef49c4ce6707903df30fa4ad77c04c6c1941073c
hyo <umdoongs@gmail.com>
2025-02-04 15:01:45 +0900

    :sparkles: dev, prod 환경 설정 기능 추가

--- config/settings/dev.py
+++ config/settings/dev.py
@@ -4,12 +4,24 @@ from .base import *
 
 DEBUG = True
 
+ENV_FILE = BASE_DIR / f"envs/local.env"
+# 필요시 export DJANGO_ENV=prod
+# .toml 파일에 config.settings.prod 변경 후 mypy 실행
+
+environ.Env.read_env(str(ENV_FILE))
+# environ.Env.read_env()  # .envs 파일을 읽어옴
+
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
+if SECRET_KEY is None:
+    raise ValueError("DJANGO_SECRET_KEY environment variable is not set.")
+
 ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 INSTALLED_APPS += [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
+    "apps.users.apps.UsersConfig"
 ]
 
 DATABASES = {
