b4b4daf554250d0f97c56f3e92eda5763bcbe61b
hyo <umdoongs@gmail.com>
2025-02-04 15:42:34 +0900

    :sparkles: User Model PermissionsMixin 추가

--- apps/users/models.py
+++ apps/users/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from apps.common.models import BaseModel
 
 
-class User(BaseModel, AbstractBaseUser):
+class User(BaseModel, AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(unique=True)
     password = models.CharField(max_length=130)
     nickname = models.CharField(max_length=20)
@@ -14,3 +14,15 @@ class User(BaseModel, AbstractBaseUser):
 
     # 로그인 시 username이 아니라 email로 로그인하게 됨(식별자가 email)
     USERNAME_FIELD = "email"
+
+    def has_perm(self, perm, obj=None) -> bool:
+        # 사용자가 superuser인 경우 Django의 모든 권한 부여
+        if self.is_superuser:
+            return True
+        return False
+
+    def has_module_perms(self, app_label) -> bool:
+        # 사용자가 superuser인 경우 모든 앱의 권한을 부여
+        if self.is_superuser:
+            return True
+        return False
