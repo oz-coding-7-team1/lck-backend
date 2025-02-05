4e61511cd37e6652d82a3a22730a2b6b035d77dc
Umdoong <umdoongs@gmail.com>
2025-02-05 18:44:19 +0900

    :bulb: OneToOne을 FK로 변경 및 코드 위치 수정

--- apps/subscriptions/models.py
+++ apps/subscriptions/models.py
@@ -9,26 +9,29 @@ from apps.users.models import User
 
 # 선수 구독 관리 모델
 class PlayerSubscription(BaseModel, SoftDeleteModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="구독한 유저 식별자")
     player = models.ForeignKey(
         Player, on_delete=models.CASCADE, related_name="subscriptions", help_text="구독한 선수 식별자"
     )
-    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="구독한 유저 식별자")
 
     class Meta:
         db_table = "player_subscription"
+        unique_together = ("user_id", "player_id") # 유저와 선수의 조합이 유니크하도록 설정
 
 
 # 팀 구독 정보를 저장하는 모델
 class TeamSubscription(BaseModel, SoftDeleteModel):
     # 구독한 유저 식별자(한명의 유저는 하나의 팀만 구독이 가능함)
-    user = models.OneToOneField(User, on_delete=models.CASCADE, help_text="구독한 유저 식별자")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, help_text="구독한 유저 식별자")
     # 구독한 팀 식별자(하나의 팀은 여러명의 구독자를 가질 수 있음)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE, related_name="subscriptions", help_text="구독한 팀 식별자")
+    team = models.ForeignKey(
+        Team, on_delete=models.CASCADE, related_name="subscriptions", help_text="구독한 팀 식별자"
+    )
 
     class Meta:
-        # 동일한 user와 team의 조합은 중복되지 않도록 함
-        unique_together = ("user_id", "team_id")
         db_table = "team_subscription"
+        unique_together = ("user_id", "team_id") # 유저와 팀의 조합이 유니크하도록 설정
+
 
     def __str__(self) -> str:
-        return f"User {self.user} - {self.team.name}"
+        return f"User {self.user} - {self.team.name}" # 객체의 문자열 표현
