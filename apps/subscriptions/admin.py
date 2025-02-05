31bb80cd45a321b46a67ef7ba71b0d05aa066832
Umdoong <umdoongs@gmail.com>
2025-02-05 16:24:50 +0900

    :sparkles: sub admin 추가

--- apps/subscriptions/admin.py
+++ apps/subscriptions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
-
+from .models import PlayerSubscription, TeamSubscription
 # Register your models here.
+@admin.register(PlayerSubscription, TeamSubscription)
+class PlayerSubscriptionAdmin(admin.ModelAdmin):
+	pass
+
+
+class TeamSubscriptionAdmin(admin.ModelAdmin):
+	pass
