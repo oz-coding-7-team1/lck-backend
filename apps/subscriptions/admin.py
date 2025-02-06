2bd9021beff443aeb24078077b9cd5753b3d30ea
Umdoong <umdoongs@gmail.com>
2025-02-06 21:02:20 +0900

    :pencil2: run black, mypy

--- apps/subscriptions/admin.py
+++ apps/subscriptions/admin.py
@@ -1,38 +1,46 @@
+from typing import Any, Tuple
+
 from django.contrib import admin
-from .models import PlayerSubscription, TeamSubscription
 from django_softdelete.admin import SoftDeletedModelAdmin
 
-class SoftDeleteListFilter(admin.SimpleListFilter):
-    title = '삭제 여부'
-    parameter_name = 'is_deleted'
+from .models import PlayerSubscription, TeamSubscription
 
-    def lookups(self, request, model_admin):
+
+class SoftDeleteListFilter(admin.SimpleListFilter):
+    title = "삭제 여부"
+    parameter_name = "deleted_at"
+
+    def lookups(self, request: Any, model_admin: Any) -> Tuple[Tuple[str, str], ...]:
         return (
-            ('True', '삭제됨'),
-            ('False', '삭제되지 않음'),
+            ("True", "삭제됨"),
+            ("False", "삭제되지 않음"),
         )
 
-    def queryset(self, request, queryset):
-        if self.value() == 'True':
+    def queryset(self, request: Any, queryset: Any) -> Any:
+        if self.value() == "True":
             return queryset.filter(deleted_at__isnull=False)
-        elif self.value() == 'False':
+        elif self.value() == "False":
             return queryset.filter(deleted_at__isnull=True)
         return queryset
 
+
 @admin.register(PlayerSubscription)
-class PlayerSubscriptionAdmin(SoftDeletedModelAdmin, admin.ModelAdmin):
-    list_display = ('user', 'player', 'deleted_at', 'restored_at')
+class PlayerSubscriptionAdmin(SoftDeletedModelAdmin, admin.ModelAdmin):  # type: ignore
+    list_display = ("user", "player", "deleted_at", "restored_at")
     list_filter = (SoftDeleteListFilter,)
 
-    def is_deleted(self, obj):
+    def is_deleted(self, obj):  # type: ignore
         return obj.deleted_at is not None
-    is_deleted.boolean = True
+
+    is_deleted.boolean = True  # type: ignore
+
 
 @admin.register(TeamSubscription)
-class TeamSubscriptionAdmin(SoftDeletedModelAdmin, admin.ModelAdmin):
-    list_display = ('user', 'team', 'deleted_at', 'restored_at')
+class TeamSubscriptionAdmin(SoftDeletedModelAdmin, admin.ModelAdmin):  # type: ignore
+    list_display = ("user", "team", "deleted_at", "restored_at")
     list_filter = (SoftDeleteListFilter,)
 
-    def is_deleted(self, obj):
+    def is_deleted(self, obj):  # type: ignore
         return obj.deleted_at is not None
-    is_deleted.boolean = True
+
+    is_deleted.boolean = True  # type: ignore
