835b9f8209b3d1d81e7eb39b47ab8264b4a3f59c
Jung <jjlee030415@gmail.com>
2025-02-06 15:55:19 +0900

    :bulb: MVP player API serializers.py에다 소셜 미디어 정보, 전체 선수 정보, 상위 10명의 선수 정보, 특정 포지션의 상위 5명의 선수 정보 추가 및 선수 프로필 반환하는 Serializer 수정

--- apps/players/serializers.py
+++ apps/players/serializers.py
@@ -1,21 +1,41 @@
-from typing import Any, Dict, List, Optional
-
 from rest_framework import serializers
 
 from .models import Player, PlayerImage, PlayerSchedule
 
 
-# Player 모델의 데이터를 직렬화하는 Serializer
+# 소셜 미디어 정보를 직렬화하는 시리얼라이저
+class PlayerSocialSerializer(serializers.Serializer[None]):
+    insta = serializers.URLField(required=False)  # 이 필드는 필수 입력이 아님
+    facebook = serializers.URLField(required=False)  # 이 필드는 필수 입력이 아님
+    youtube = serializers.URLField(required=False)  # 이 필드는 필수 입력이 아님
+    X = serializers.URLField(required=False)  # 이 필드는 필수 입력이 아님
+
+
+# 전체 선수 정보를 직렬화하는 시리얼라이저
 class PlayerSerializer(serializers.ModelSerializer[Player]):
+    social = PlayerSocialSerializer()  # 소셜 미디어 정보를 포함
+
     class Meta:
         model = Player
-        fields = "__all__"  # 모델의 모든 필드를 포함
+        fields = ["id", "nickname", "realname", "position", "social"]
+
+
+# 상위 10명의 선수 정보를 직렬화하는 시리얼라이저
+class PlayerTopSerializer(serializers.ModelSerializer[Player]):
+    class Meta:
+        model = Player
+        fields = ["id", "nickname", "realname"]
+
+
+# 특정 포지션의 상위 5명의 선수 정보를 직렬화하는 시리얼라이저
+class PlayerPositionSerializer(serializers.ModelSerializer[Player]):
+    class Meta:
+        model = Player
+        fields = ["id", "nickname", "position"]
 
 
 # 선수 프로필 정보를 반환하는 Serializer
 class PlayerProfileSerializer(serializers.ModelSerializer[Player]):
-    images = serializers.SerializerMethodField()  # 선수 이미지 목록을 추가로 포함
-    team_id = serializers.SerializerMethodField()  # 소속 팀의 ID를 추가
 
     class Meta:
         model = Player
@@ -30,25 +50,7 @@ class PlayerProfileSerializer(serializers.ModelSerializer[Player]):
             "debut_date",  # 데뷔 날짜
             "social",  # 소셜 미디어 정보
             "agency",  # 소속 에이전시
-            "images",  # 이미지 목록
         ]
-
-    def get_images(self, obj: Player) -> List[Dict[str, Any]]:
-        """
-        해당 선수의 이미지를 반환하는 메서드.
-        :param obj: Player 객체
-        :return: 이미지 정보를 담은 딕셔너리 리스트
-        """
-        images = PlayerImage.objects.filter(player=obj)  # 해당 선수의 이미지를 필터링
-        return list(PlayerImageSerializer(images, many=True).data)  # 직렬화 후 반환
-
-    def get_team_id(self, obj: Player) -> Optional[int]:
-        """
-        선수의 소속 팀 ID를 반환하는 메서드.
-        :param obj: Player 객체
-        :return: 팀 ID (소속 팀이 있는 경우) 또는 None (소속 팀이 없는 경우)
-        """
-        return obj.team.id if obj.team else None  # 선수의 소속 팀이 있는 경우 팀 ID 반환, 없는 경우 None 반환
 
 
 # PlayerImage 모델의 데이터를 직렬화하는 Serializer
