348c0c8a58582d374584f1a0d225424098a82ce0
Umdoong <umdoongs@gmail.com>
2025-02-06 20:47:56 +0900

    :bulb: 하드 딜리트 코드 수정

--- apps/subscriptions/management/commands/hard_delete_old_subscriptions.py
+++ apps/subscriptions/management/commands/hard_delete_old_subscriptions.py
@@ -14,11 +14,16 @@ class Command(BaseCommand):
 			"""
 
     def handle(self, *args: Any, **options: Any) -> None:
-        threshold = now() - timedelta(days=3)
-
         # 3일 이상 지난 soft delete된 row 삭제
-        deleted_players = PlayerSubscription.objects.filter(deleted_at__lte=threshold).delete()
-        deleted_teams = TeamSubscription.objects.filter(deleted_at__lte=threshold).delete()
+        # 소프트 딜리트된 레코드를 포함한 모든 레코드를 가져옵니다.
+        deleted_players = PlayerSubscription.deleted_objects.filter(deleted_at__lte=now() - timedelta(days=3))
+        deleted_teams = TeamSubscription.deleted_objects.filter(deleted_at__lte=now() - timedelta(days=3))
 
-        self.stdout.write(self.style.SUCCESS(f"선수 구독 {deleted_players[0]}개 삭제 완료"))
-        self.stdout.write(self.style.SUCCESS(f"팀 구독 {deleted_teams[0]}개 삭제 완료"))
+        deleted_players_count = deleted_players.count()
+        deleted_teams_count = deleted_teams.count()
+
+        deleted_players.hard_delete()
+        deleted_teams.hard_delete()
+
+        self.stdout.write(self.style.SUCCESS(f"선수 구독 {deleted_players_count}개 삭제 완료"))
+        self.stdout.write(self.style.SUCCESS(f"팀 구독 {deleted_teams_count}개 삭제 완료"))
