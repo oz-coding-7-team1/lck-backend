54ebc63677fa787c9f11501cf8972d05c24233e4
Umdoong <umdoongs@gmail.com>
2025-02-06 18:15:22 +0900

    :recycle: db 환경 변수명 변경

--- config/settings/base.py
+++ config/settings/base.py
@@ -68,7 +68,6 @@ PACKAGE = [
     "taggit_templatetags2",
 ]
 
-
 INSTALLED_APPS = DJANGO_APPS + PACKAGE
 
 MIDDLEWARE = [
@@ -106,13 +105,13 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME"),
-        "USER": os.getenv("DB_USER"),
-        "PASSWORD": os.getenv("DB_PASSWORD"),
-        "HOST": os.getenv("DB_HOST", "localhost"),
-        "PORT": os.getenv("DB_PORT", "5432"),
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
