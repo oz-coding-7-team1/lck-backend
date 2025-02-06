41626e0ac97188f7520503c20c9f82c5631ae762
Umdoong <umdoongs@gmail.com>
2025-02-06 20:48:23 +0900

    :sparkles: 장고 어드민 페이지 소프트 딜리트도 볼 수 있게 수정

--- apps/subscriptions/admin.py
+++ apps/subscriptions/admin.py
@@ -1,13 +1,38 @@
 from django.contrib import admin
-
 from .models import PlayerSubscription, TeamSubscription
+from django_softdelete.admin import SoftDeletedModelAdmin
 
+class SoftDeleteListFilter(admin.SimpleListFilter):
+    title = '삭제 여부'
+    parameter_name = 'is_deleted'
 
-# Register your models here.
-@admin.register(PlayerSubscription, TeamSubscription)
-class PlayerSubscriptionAdmin(admin.ModelAdmin):  # type: ignore
-    pass
+    def lookups(self, request, model_admin):
+        return (
+            ('True', '삭제됨'),
+            ('False', '삭제되지 않음'),
+        )
 
+    def queryset(self, request, queryset):
+        if self.value() == 'True':
+            return queryset.filter(deleted_at__isnull=False)
+        elif self.value() == 'False':
+            return queryset.filter(deleted_at__isnull=True)
+        return queryset
 
-class TeamSubscriptionAdmin(admin.ModelAdmin):  # type: ignore
-    pass
+@admin.register(PlayerSubscription)
+class PlayerSubscriptionAdmin(SoftDeletedModelAdmin, admin.ModelAdmin):
+    list_display = ('user', 'player', 'deleted_at', 'restored_at')
+    list_filter = (SoftDeleteListFilter,)
+
+    def is_deleted(self, obj):
+        return obj.deleted_at is not None
+    is_deleted.boolean = True
+
+@admin.register(TeamSubscription)
+class TeamSubscriptionAdmin(SoftDeletedModelAdmin, admin.ModelAdmin):
+    list_display = ('user', 'team', 'deleted_at', 'restored_at')
+    list_filter = (SoftDeleteListFilter,)
+
+    def is_deleted(self, obj):
+        return obj.deleted_at is not None
+    is_deleted.boolean = True
