e18c6a667470b8f6ae6a419250d46e1d5a552114
Jung <jjlee030415@gmail.com>
2025-02-07 18:20:54 +0900

    :bulb: views.py 코드 수정

--- apps/players/urls.py
+++ apps/players/urls.py
@@ -5,8 +5,8 @@ from .views import (
     PlayerImageList,
     PlayerList,
     PlayerScheduleList,
-    position_top,
-    top_players,
+    PositionTop,
+    TopPlayers,
 )
 
 urlpatterns = [
@@ -15,9 +15,9 @@ urlpatterns = [
     # Player 프로필 조회
     path("players/<int:pk>/", PlayerDetail.as_view(), name="player-detail"),
     # 구독 수가 많은 상위 10명의 선수 정보 조회
-    path("players/top/", top_players, name="top-players"),
+    path("players/top/", TopPlayers.as_view(), name="top-players"),
     # 특정 포지션의 구독 수가 많은 상위 5명의 선수 정보 조회
-    path("players/position_top/", position_top, name="position-top"),
+    path("players/position_top/", PositionTop.as_view(), name="position-top"),
     # 특정 선수의 이미지조회
     path("players/<int:pk>/images/", PlayerImageList.as_view(), name="player-image-list"),
     # 특정 선수의 스케줄 조회
--- apps/players/views.py
+++ apps/players/views.py
@@ -3,8 +3,7 @@ from typing import Any, Optional
 from django.db.models import Count
 from django.http import Http404
 from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.filters import SearchFilter
+from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -19,13 +18,11 @@ from .serializers import (
 )
 
 
-# 전체 Player 조회 모델의 목록을 처리하는 API 뷰
+# 전체 Player 조회
 class PlayerList(APIView):
-    # 검색 기능을 사용하도록 설정
-    # SearchFilter를 사용하면 URL 쿼리 파라미터를 통해 검색할 수 있다.
-    filter_backends = [SearchFilter]
-    # 검색할 수 있는 필드를 지정
-    search_fields = ["nickname", "realname", "position", "team_id"]
+    """
+    타입 어노테이션은 변수나 함수의 타입 정보를 명시하여 코드의 가독성과 안정성을 높여주며, 정적 타입 검사 도구와 IDE의 기능을 향상시킴
+    """
 
     def get(self, request: Any, format: Optional[str] = None) -> Response:
         # 모든 Player 객체를 데이터베이스에서 조회
@@ -37,81 +34,42 @@ class PlayerList(APIView):
         return Response(serializer.data)
 
 
-# Player 프로필 모델의 개별 객체를 처리하는 API 뷰
+# Player 프로필 조회
 class PlayerDetail(APIView):
-    def get_object(self, pk: int) -> Player:
-        # 주어진 pk에 해당하는 Player 객체를 반환, 없으면 404 에러 발생
+    def get(self, request: Request, pk: int, format: Optional[str] = None) -> Response:
         try:
-            # Player 모델에서 기본 키가 pk와 일치하는 객체를 조회
-            return Player.objects.get(pk=pk)
+            # 주어진 pk에 해당하는 Player 객체를 조회
+            player = Player.objects.get(pk=pk)
         # 해당 객체가 존재하지 않으면 Player.DoesNotExist 예외가 발생
+        # DoesNotExist는 "해당 객체가 존재하지 않음"을 나타내는 Django의 기본 예외이며
+        # 이를 통해 조회 실패 시 적절한 에러 처리를 할 수 있음
         except Player.DoesNotExist:
+            # 객체가 존재하지 않으면 404 에러 발생
             raise Http404
 
-    def get(self, request: Any, pk: int, format: Optional[str] = None) -> Response:
-        # 메서드를 호출하여 pk에 해당하는 Player 객체를 조회
-        player = self.get_object(pk)
         # 조회된 Player 객체를 PlayerProfileSerializer를 사용하여 직렬화
         serializer = PlayerProfileSerializer(player)
         # 직렬화된 데이터를 Response 객체에 담아 클라이언트에게 반환
         return Response(serializer.data)
 
 
-# 구독 수가 많은 상위 10명의 선수 정보를 반환하는 API 뷰
-@api_view(["GET"])
-def top_players(request: Any) -> Response:
-    try:
-        # 각 Player 객체에 대해 연결된 subscriptions의 개수를 어노테이션하여
-        # subscriber_count 필드에 저장한 후, 이를 기준으로 내림차순 정렬하고 상위 10개를 조회
-        top_players = Player.objects.annotate(subscriber_count=Count("subscriptions")).order_by("-subscriber_count")[
-            :10
-        ]
-        # 조회된 top_players 객체들을 PlayerTopSerializer를 사용하여 직렬화
-        # many=True는 여러 개의 객체를 직렬화할 때 사용
-        serializer = PlayerTopSerializer(top_players, many=True)
-        # 직렬화된 데이터를 Response 객체로 반환
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    except Exception as e:
-        # 에러가 발생하면 에러 메시지와 함께 500 에러를 반환
-        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-# 특정 포지션의 구독 수가 많은 상위 5명의 선수 정보를 반환하는 API 뷰
-@api_view(["GET"])
-def position_top(request: Any) -> Response:
-    # 쿼리 파라미터로 포지션을 가져옴
-    position = request.query_params.get("position")
-    if not position:
-        return Response({"error": "Position parameter is required"}, status=status.HTTP_400_BAD_REQUEST)
-    try:
-        # 해당 포지션의 Player 객체에 대해 연결된 subscriptions의 개수를 어노테이션하여
-        # subscriber_count 필드에 저장한 후, 이를 기준으로 내림차순 정렬하고 상위 5개를 조회
-        top_players = (
-            # 데이터베이스에서 position 값이 쿼리 파라미터로 전달된 값과 일치하는 Player 객체들만 선택
-            Player.objects.filter(position=position)
-            # subscriber_count 필드에 저장한 후, 이를 기준으로 내림차순 정렬하고 상위 5개를 조회
-            .annotate(subscriber_count=Count("subscriptions")).order_by("-subscriber_count")[:5]
-        )
-        # 조회된 top_players 객체들을 PlayerPositionSerializer를 사용하여 직렬화
-        serializer = PlayerPositionSerializer(top_players, many=True)
-        # 직렬화된 데이터를 Response 객체로 반환
-        return Response(serializer.data, status=status.HTTP_200_OK)
-    except Exception as e:
-        # 에러가 발생하면 에러 메시지와 함께 500 에러를 반환
-        return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
-
-
-# 특정 선수의 이미지를 처리하는 API 뷰
-class PlayerImageList(APIView):
-    def get(self, request: Any, pk: int, format: Optional[str] = None) -> Response:
-        # Player ID가 없으면 에러 메시지 반환
-        if pk is None:
-            return Response({"error": "Player ID is required"}, status=status.HTTP_400_BAD_REQUEST)
+# 구독 수가 많은 상위 10명의 선수 조회
+class TopPlayers(APIView):
+    def get(self, request: Request, format: Optional[str] = None) -> Response:
         try:
-            # 주어진 pk에 해당하는 Player의 이미지 객체들을 데이터베이스에서 조회
-            images = PlayerImage.objects.filter(player_id=pk)
-            # 조회된 images 객체들을 PlayerImageSerializer를 사용하여 직렬화
-            serializer = PlayerImageSerializer(images, many=True)
+            """
+            어노테이션은 QuerySet에 새로운 필드(계산된 값)를 추가하기 위한 메서드,
+            subscriber_count는 각 선수의 구독자 수를 나타내는 동적으로 추가된 필드,
+            Count("subscriptions")는 각 Player 객체와 연결된 subscriptions의 개수를 계산
+            """
+            # 각 Player 객체에 대해 연결된 subscriptions의 개수를 어노테이션하여
+            # subscriber_count 필드에 저장한 후, 이를 기준으로 내림차순 정렬하고 상위 10개를 조회
+            top_players = Player.objects.annotate(subscriber_count=Count("subscriptions")).order_by(
+                "-subscriber_count"
+            )[:10]
+            # 조회된 top_players 객체들을 PlayerTopSerializer를 사용하여 직렬화
+            # many=True는 여러 개의 객체를 직렬화할 때 사용
+            serializer = PlayerTopSerializer(top_players, many=True)
             # 직렬화된 데이터를 Response 객체로 반환
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
@@ -119,19 +77,69 @@ class PlayerImageList(APIView):
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-# 특정 선수의 스케줄을 처리하는 API 뷰
-class PlayerScheduleList(APIView):
-    def get(self, request: Any, pk: int, format: Optional[str] = None) -> Response:
-        # Player ID가 없으면 에러 메시지 반환
-        if pk is None:
-            return Response({"error": "Player ID is required"}, status=status.HTTP_400_BAD_REQUEST)
+# 특정 포지션의 구독 수가 많은 상위 5명의 선수 조회
+class PositionTop(APIView):
+    def get(self, request: Request, format: Optional[str] = None) -> Response:
+        # 클라이언트의 요청에서 쿼리 파라미터로 전달된 'position' 값을 가져옴
+        position = request.query_params.get("position")
+        if not position:
+            # 만약 'position' 값이 제공되지 않았다면,
+            # 클라이언트에게 400 오류와 함께 에러 메시지를 반환
+            return Response({"error": "Position parameter is required"}, status=status.HTTP_400_BAD_REQUEST)
         try:
-            # 주어진 pk에 해당하는 Player의 스케줄 객체들을 데이터베이스에서 조회
-            schedules = PlayerSchedule.objects.filter(player_id=pk)
-            # 조회된 schedules 객체들을 PlayerScheduleSerializer를 사용하여 직렬화
-            serializer = PlayerScheduleSerializer(schedules, many=True)
-            # 직렬화된 데이터를 Response 객체로 반환
+            # Player 모델에서, 포지션 필드가 요청한 값과 일치하는 선수들만 필터링함
+            # 필터링된 Player 객체들에 대해, 연결된 subscriptions의 개수를 계산하여
+            # subscriber_count라는 이름의 동적 필드로 추가
+            # 이후, subscriber_count 필드를 기준으로 내림차순 정렬한 후 상위 5명을 선택
+            top_players = (
+                Player.objects.filter(position=position)
+                .annotate(subscriber_count=Count("subscriptions"))
+                .order_by("-subscriber_count")[:5]
+            )
+
+            # 조회된 Player 객체들을 PlayerPositionSerializer를 사용하여 직렬화
+            # many=True는 여러 개의 객체를 직렬화할 때 사용
+            serializer = PlayerPositionSerializer(top_players, many=True)
+
+            # 직렬화된 데이터를 HTTP 200상태 코드와 함께 Response 객체로 반환
             return Response(serializer.data, status=status.HTTP_200_OK)
         except Exception as e:
-            # 에러가 발생하면 에러 메시지와 함께 500 에러를 반환
+            # 예외 발생 시, 에러 메시지와 함께 HTTP 500상태 코드를 반환
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+# 특정 선수의 이미지를 조회
+class PlayerImageList(APIView):
+    """
+    특정 선수의 이미지들을 반환
+    선수 프로필과 별도로 이미지 목록만 조회
+    """
+
+    def get(self, request: Request, pk: int, format: Optional[str] = None) -> Response:
+        try:
+            # URL에 전달된 pk를 이용하여 해당 선수의 이미지들을 조회
+            images = PlayerImage.objects.filter(player_id=pk)
+            # 조회된 이미지들을 PlayerImageSerializer를 사용해 직렬화
+            # many=True는 여러 개의 객체를 직렬화할 때 사용
+            serializer = PlayerImageSerializer(images, many=True)
+            # 직렬화된 데이터를 HTTP 200 상태와 함께 반환
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            # 예외 발생 시, 에러 메시지와 함께 HTTP 500상태 코드를 반환
+            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+# 특정 선수의 스케줄 조회
+class PlayerScheduleList(APIView):
+    def get(self, request: Request, pk: int, format: Optional[str] = None) -> Response:
+        try:
+            # 주어진 pk에 해당하는 Player의 스케줄들을 조회
+            schedules = PlayerSchedule.objects.filter(player_id=pk)
+            # 조회된 스케줄들을 PlayerScheduleSerializer를 사용해 직렬화
+            # many=True는 여러 개의 객체를 직렬화할 때 사용
+            serializer = PlayerScheduleSerializer(schedules, many=True)
+            # 직렬화된 데이터를 HTTP 200 상태와 함께 반환
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Exception as e:
+            # 예외 발생 시, 에러 메시지와 함께 HTTP 500상태 코드를 반환
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
