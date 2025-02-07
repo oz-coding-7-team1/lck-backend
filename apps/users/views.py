770a87770ac3f153fcb22eb28d4cac5146dfe9d3
BlueLight <dlrkdmf10@gmail.com>
2025-02-07 14:11:06 +0900

    :sparkles: Add change password API

--- apps/users/views.py
+++ apps/users/views.py
@@ -95,3 +95,27 @@ class UserLogoutView(APIView):
             return Response({"detail": "로그아웃에 성공하였습니다."}, status=status.HTTP_204_NO_CONTENT)
         except Exception as e:
             return Response({"error": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+# 비밀번호 변경
+class ChangePasswordView(APIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+
+    def post(self, request):
+        user = request.user
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+
+        # 현재 비밀번호 확인
+        if not user.check_password(old_password):
+            raise PermissionDenied("현재 비밀번호가 일치하지 않습니다.")
+
+        try:
+            validate_password(new_password)
+        except Exception as e:
+            raise ParseError("새 비밀번호가 유효하지 않습니다. " + str(e))
+
+        user.set_password(new_password)
+        user.save()
+        return Response({"detail": "비밀번호가 성공적으로 변경되었습니다."}, status=status.HTTP_200_OK)
