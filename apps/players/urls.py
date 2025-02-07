ee3f823c6710ec2edc293df442f8694efabbc5eb
Jung <jjlee030415@gmail.com>
2025-02-07 15:37:41 +0900

    :bulb: players/urls.py 수정

--- apps/players/urls.py
+++ apps/players/urls.py
@@ -11,15 +11,15 @@ from .views import (
 
 urlpatterns = [
     # 전체 Player 조회
-    path("api/v1/players/", PlayerList.as_view(), name="player-list"),
+    path("players/", PlayerList.as_view(), name="player-list"),
     # Player 프로필 조회
-    path("api/v1/players/<int:pk>/", PlayerDetail.as_view(), name="player-detail"),
+    path("players/<int:pk>/", PlayerDetail.as_view(), name="player-detail"),
     # 구독 수가 많은 상위 10명의 선수 정보 조회
-    path("api/v1/players/top/", top_players, name="top-players"),
+    path("players/top/", top_players, name="top-players"),
     # 특정 포지션의 구독 수가 많은 상위 5명의 선수 정보 조회
-    path("api/v1/players/position_top/", position_top, name="position-top"),
+    path("players/position_top/", position_top, name="position-top"),
     # 특정 선수의 이미지조회
-    path("api/v1/players/<int:pk>/images/", PlayerImageList.as_view(), name="player-image-list"),
+    path("players/<int:pk>/images/", PlayerImageList.as_view(), name="player-image-list"),
     # 특정 선수의 스케줄 조회
-    path("api/v1/player/<int:pk>/schedule/", PlayerScheduleList.as_view(), name="player-schedule"),
+    path("player/<int:pk>/schedule/", PlayerScheduleList.as_view(), name="player-schedule"),
 ]
