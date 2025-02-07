242c64445f1c0f4c94eda87d08093baf48109ebb
BlueLight <dlrkdmf10@gmail.com>
2025-02-07 14:09:10 +0900

    ✨ Add users urls
    Add users urls

---
+++ apps/users/urls.py
@@ -0,0 +1,24 @@
+from django.urls import path
+from apps.users.views import UserRegisterView, UserLoginView, UserLogoutView, MyPageView, ChangePasswordView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
+urlpatterns = [
+    path("obtain/", TokenObtainPairView.as_view(), name="token_obtain"),
+    path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("verify/", TokenVerifyView.as_view(), name="token_verify"),
+
+    path("signup/", UserRegisterView.as_view(), name="signup"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
+    # path("Withdrawal/",),
+
+    path("user/mypage/", MyPageView.as_view(), name="mypage"),
+    path("user/password/", ChangePasswordView.as_view(), name="change-password"),
+    # path("user/image/",),
+    # path("user/image/<int:pk>",),
+    # path("user/<int:pk>",),
+]
