140ae4e61a5e3eb6547acc48ca12b2a280122987
Jung <jjlee030415@gmail.com>
2025-02-09 23:48:47 +0900

    :sparkles: 선수 비활성화 PATCH, 선수 삭제 DELETE, 선수 스케줄 전체 조회랑 상세조회로 분리, 선수 스케줄 생성 POST, 특정 선수 스케줄 상세 수정 PATCH, 특정 선수 스케줄 상세 삭제 DELETE 추가

--- apps/players/views.py
+++ apps/players/views.py
@@ -36,7 +36,7 @@ class PlayerList(APIView):
             return [IsAuthenticated(), IsAdminUser()]
         return []
 
-    # 전체 Player 조회
+    # 전체 선수 조회
     def get(self, request: Any, format: Optional[str] = None) -> Response:
         # 모든 Player 객체를 데이터베이스에서 조회
         players = Player.objects.all()
@@ -46,7 +46,7 @@ class PlayerList(APIView):
         # 직렬화된 데이터를 Response 객체로 반환
         return Response(serializer.data)
 
-    # Player 등록
+    # 선수 등록
     def post(self, request: Request, format: Optional[str] = None) -> Response:
         # 클라이언트가 전송한 JSON 데이터를 기반으로 PlayerCreateSerializer를 초기화
         serializer = PlayerCreateSerializer(data=request.data)
@@ -60,13 +60,18 @@ class PlayerList(APIView):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
+# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
+
+
 class PlayerDetail(APIView):
-    # Player 프로필 조회
+
     def get_permissions(self) -> List[Any]:
-        if self.request.method == "POST":
+        if self.request.method in ["PUT", "PATCH", "DELETE"]:
             return [IsAuthenticated(), IsAdminUser()]
         return []
 
+    # 선수 프로필 조회
     def get(self, request: Request, pk: int, format: Optional[str] = None) -> Response:
         try:
             # 주어진 pk에 해당하는 Player 객체를 조회
@@ -83,7 +88,7 @@ class PlayerDetail(APIView):
         # 직렬화된 데이터를 Response 객체에 담아 클라이언트에게 반환
         return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # Player 프로필 수정
+    # 선수 프로필 수정
     def put(self, request: Request, pk: int, format: Optional[str] = None) -> Response:
         # 주어진 pk를 기반으로 Player 객체를 데이터베이스에서 조회
         try:
@@ -105,6 +110,47 @@ class PlayerDetail(APIView):
 
         # 데이터 유효성 검사에 실패한 경우, 에러 메시지와 함께 HTTP 400 상태 코드를 반환
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # 선수 비활성화
+    def patch(self, request: Request, pk: int, format: Optional[str] = None) -> Response:
+        try:
+            # 주어진 pk를 기반으로 Player 객체를 데이터베이스에서 조회
+            player = Player.objects.get(pk=pk)
+        except Player.DoesNotExist:
+            # 객체가 없으면 NotFound 예외를 발생시켜 404 응답 반환
+            raise NotFound(detail="해당 플레이어를 찾을 수 없습니다.")
+
+        # 요청 본문에 "is_active" 값이 False 인지 확인 (비활성화 요청)
+        if request.data.get("is_active") is False:
+            # Player 객체의 is_active 필드를 False로 변경
+            player.is_active = False
+            player.save()
+            # 성공 메시지와 함께 HTTP 204 No Content 상태 코드를 반환
+            return Response({"detail": "선수 비활성화 완료"}, status=status.HTTP_204_NO_CONTENT)
+        else:
+            # 요청 데이터가 올바르지 않으면 에러 메시지와 함께 HTTP 400 상태 코드 반환
+            return Response(
+                {"error": "요청 데이터가 올바르지 않습니다. is_active 값은 반드시 False여야 합니다."},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
+
+    # 선수 삭제
+    def delete(self, request: Request, pk: int, format: Optional[str] = None) -> Response:
+        try:
+            # 주어진 pk를 기반으로 Player 객체를 데이터베이스에서 조회
+            player = Player.objects.get(pk=pk)
+        except Player.DoesNotExist:
+            # 객체가 없으면 NotFound 예외를 발생시켜 404 응답 반환
+            raise NotFound(detail="해당 플레이어를 찾을 수 없습니다.")
+
+        # soft delete 방식으로 Player 객체 삭제 (실제 삭제가 아닌 상태 변경)
+        player.delete()
+        # 삭제 완료 메시지와 함께 HTTP 204 No Content 상태 코드를 반환
+        return Response({"detail": "선수 삭제 완료"}, status=status.HTTP_204_NO_CONTENT)
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
 
 
 # 구독 수가 많은 상위 10명의 선수 조회
@@ -129,6 +175,10 @@ class TopPlayers(APIView):
         except Exception as e:
             # 에러가 발생하면 에러 메시지와 함께 500 에러를 반환
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
 
 
 # 특정 포지션의 구독 수가 많은 상위 5명의 선수 조회
@@ -162,17 +212,95 @@ class PositionTop(APIView):
             return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
 
-# 특정 선수의 스케줄 조회
+# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
+
+
 class PlayerScheduleList(APIView):
-    def get(self, request: Request, pk: int, format: Optional[str] = None) -> Response:
+    def get_permissions(self) -> List[Any]:
+        if self.request.method == "POST":
+            return [IsAuthenticated(), IsAdminUser()]
+        return []
+
+    # 특정 선수의 스케줄 목록 조회
+    def get(self, request: Request, player_id: int, format: Optional[str] = None) -> Response:
+        # 선수 ID에 해당하는 모든 스케줄 객체들을 필터링
+        schedules = PlayerSchedule.objects.filter(player_id=player_id)
+        # 조회한 스케줄 객체들을 PlayerScheduleSerializer를 이용하여 직렬화
+        # many=True를 지정하여 여러 객체를 리스트 형태로 직렬화
+        serializer = PlayerScheduleSerializer(schedules, many=True)
+        # 직렬화된 데이터를 HTTP 200 상태 코드와 함께 클라이언트에 반환
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # 특정 선수의 스케줄 목록 생성
+    def post(self, request: Request, player_id: int, format: Optional[str] = None) -> Response:
+        # 클라이언트가 전송한 데이터를 복사
+        data = request.data.copy()
+        # URL에서 전달된 선수 ID를 사용하여 데이터의 'player' 필드를 설정
+        data["player"] = player_id
+        # PlayerScheduleSerializer를 사용하여 데이터의 유효성을 검사하고 직렬화
+        serializer = PlayerScheduleSerializer(data=data)
+        if serializer.is_valid():
+            # 데이터가 유효하면 새 스케줄 객체를 생성
+            serializer.save()
+            # 생성 성공 메시지와 함께 HTTP 201 상태 코드를 반환
+            return Response({"detail": "선수 스케줄 생성 완료"}, status=status.HTTP_201_CREATED)
+        # 데이터 유효성 검사에 실패하면 오류 메시지와 함께 HTTP 400 상태 코드를 반환
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# -----------------------------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------------------------
+
+
+class PlayerScheduleDetail(APIView):
+    def get_permissions(self) -> List[Any]:
+        if self.request.method in ["PATCH", "DELETE"]:
+            return [IsAuthenticated(), IsAdminUser()]
+        return []
+
+    # 특정 선수 스케줄 상세 조회
+    def get(self, request: Request, player_id: int, schedule_id: int, format: Optional[str] = None) -> Response:
         try:
-            # 주어진 pk에 해당하는 Player의 스케줄들을 조회
-            schedules = PlayerSchedule.objects.filter(player_id=pk)
-            # 조회된 스케줄들을 PlayerScheduleSerializer를 사용해 직렬화
-            # many=True는 여러 개의 객체를 직렬화할 때 사용
-            serializer = PlayerScheduleSerializer(schedules, many=True)
-            # 직렬화된 데이터를 HTTP 200 상태와 함께 반환
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        except Exception as e:
-            # 예외 발생 시, 에러 메시지와 함께 HTTP 500상태 코드를 반환
-            return Response({"error": str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            # 데이터베이스에서 선수 ID와 스케줄 ID가 모두 일치하는 스케줄 객체를 조회
+            schedule = PlayerSchedule.objects.get(pk=schedule_id, player_id=player_id)
+        except PlayerSchedule.DoesNotExist:
+            # 만약 해당 스케줄 객체가 존재하지 않으면, 404 Not Found 예외를 발생
+            raise NotFound(detail="해당 스케줄을 찾을 수 없습니다.")
+
+            # 조회한 스케줄 객체를 직렬화하여 JSON 형태로 변환
+        serializer = PlayerScheduleSerializer(schedule)
+
+        # 직렬화된 데이터를 포함하는 응답 객체를 HTTP 200 상태 코드와 함께 반환
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    # 특정 선수 스케줄 상세 수정
+    def patch(self, request: Request, player_id: int, schedule_id: int, format: Optional[str] = None) -> Response:
+        try:
+            # 데이터베이스에서 선수 ID와 스케줄 ID가 모두 일치하는 스케줄 객체를 조회
+            schedule = PlayerSchedule.objects.get(pk=schedule_id, player_id=player_id)
+        except PlayerSchedule.DoesNotExist:
+            # 해당 스케줄 객체가 존재하지 않으면 404 Not Found 예외를 발생
+            raise NotFound(detail="해당 스케줄을 찾을 수 없습니다.")
+        # 전달받은 데이터로 부분 업데이트를 위한 serializer를 초기화합니다.
+        serializer = PlayerScheduleSerializer(schedule, data=request.data, partial=True)
+        if serializer.is_valid():
+            # 데이터가 유효하면 객체를 업데이트하고 저장
+            serializer.save()
+            # 업데이트 성공 메시지와 함께 HTTP 200 상태 코드를 반환
+            return Response({"detail": "선수 스케줄 수정 완료"}, status=status.HTTP_200_OK)
+        # 데이터 유효성 검사에 실패하면 오류 메시지와 함께 HTTP 400 상태 코드를 반환
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    # 특정 선수 스케줄 상세 삭제
+    def delete(self, request: Request, player_id: int, schedule_id: int, format: Optional[str] = None) -> Response:
+        try:
+            # 데이터베이스에서 선수 ID와 스케줄 ID가 모두 일치하는 스케줄 객체를 조회
+            schedule = PlayerSchedule.objects.get(pk=schedule_id, player_id=player_id)
+        except PlayerSchedule.DoesNotExist:
+            # 해당 스케줄 객체가 존재하지 않으면 404 Not Found 예외를 발생
+            raise NotFound(detail="해당 스케줄을 찾을 수 없습니다.")
+        # 조회된 스케줄 객체를 삭제
+        schedule.delete()
+        # 삭제 완료 메시지와 함께 HTTP 204 상태 코드를 반환
+        return Response({"detail": "선수 스케줄 삭제 완료"}, status=status.HTTP_204_NO_CONTENT)
