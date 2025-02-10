926ef21d264f93c71091ca6d3d12aca36611343b
Umdoong <umdoongs@gmail.com>
2025-02-10 15:35:40 +0900

    :bulb: sub urls 세분화

--- apps/subscriptions/urls.py
+++ apps/subscriptions/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
 
-from .views import PlayerSubscriptionView, TeamSubscriptionView
+from .views import (
+    PlayerSubscriptionCountView,
+    PlayerSubscriptionDetailView,
+    PlayerSubscriptionView,
+    TeamSubscriptionCountView,
+    TeamSubscriptionDetailView,
+    TeamSubscriptionView,
+)
 
 urlpatterns = [
     path("player/<int:player_id>/", PlayerSubscriptionView.as_view(), name="player_subscription"),
+    path("player/choeae/", PlayerSubscriptionDetailView.as_view(), name="player_subscription_detail"),
+    path("player/<int:player_id>/count", PlayerSubscriptionCountView.as_view(), name="player_subscription_count"),
     path("team/<int:team_id>/", TeamSubscriptionView.as_view(), name="team_subscription"),
+    path("team/choeae/", TeamSubscriptionDetailView.as_view(), name="team_subscription_detail"),
+    path("team/<int:team_id>/count", TeamSubscriptionCountView.as_view(), name="team_subscription_count"),
 ]
