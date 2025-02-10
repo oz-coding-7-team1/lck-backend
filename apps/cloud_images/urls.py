54397a50fbc7741a200b2a6e83463d400eeb6d8c
Umdoong <umdoongs@gmail.com>
2025-02-10 17:37:33 +0900

    :sparkles: cloud_images app views, tests 추가

--- apps/cloud_images/urls.py
+++ apps/cloud_images/urls.py
@@ -1,3 +1,7 @@
 from django.urls import path
+from .views import PlayerImageListView, TeamImageListView
 
-# urlpatterns = []
+urlpatterns = [
+	path('<player_id>/images/', PlayerImageListView.as_view(), name='player_image_list'),
+	path('<team_id>/images/', TeamImageListView.as_view(), name='team_image_list'),
+]
--- apps/cloud_images/views.py
+++ apps/cloud_images/views.py
@@ -1,1 +1,38 @@
-# 나중에 S3에 이미지 추가하고 URL을 DB에 저장하는 코드 만들 때 사용(utils.py도 그 때를 위해 생성)
+from django.shortcuts import get_object_or_404
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status
+
+from apps.players.models import Player
+from apps.teams.models import Team
+from .models import PlayerImage, TeamImage  # 이미지 모델 import
+
+
+class PlayerImageListView(APIView):
+    """특정 선수의 모든 이미지를 가져오는 API"""
+
+    def get(self, request, player_id):
+        player = get_object_or_404(Player, id=player_id)
+        images = PlayerImage.objects.filter(player=player, deleted_at__isnull=True)
+
+        image_list = [{"type": img.type, "url": img.url} for img in images]
+
+        return Response(
+            {"player": player.realname, "images": image_list},
+            status=status.HTTP_200_OK,
+        )
+
+
+class TeamImageListView(APIView):
+    """특정 팀의 모든 이미지를 가져오는 API"""
+
+    def get(self, request, team_id):
+        team = get_object_or_404(Team, id=team_id)
+        images = TeamImage.objects.filter(team=team, deleted_at__isnull=True)
+
+        image_list = [{"type": img.type, "url": img.url} for img in images]
+
+        return Response(
+            {"team": team.name, "images": image_list},
+            status=status.HTTP_200_OK,
+        )
