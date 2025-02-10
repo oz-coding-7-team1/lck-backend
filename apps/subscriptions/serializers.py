d04fb6bb157682e0a7e81cc05a90b4e99510943b
Umdoong <umdoongs@gmail.com>
2025-02-10 15:34:40 +0900

    :bulb: sub serializer type hint 추가

--- apps/subscriptions/serializers.py
+++ apps/subscriptions/serializers.py
@@ -3,13 +3,13 @@ from rest_framework import serializers
 from .models import PlayerSubscription, TeamSubscription
 
 
-class PlayerSubscriptionSerializer(serializers.ModelSerializer):  # type: ignore
+class PlayerSubscriptionSerializer(serializers.ModelSerializer[PlayerSubscription]):
     class Meta:
         model = PlayerSubscription
         fields = "__all__"  # 모든 필드를 포함
 
 
-class TeamSubscriptionSerializer(serializers.ModelSerializer):  # type: ignore
+class TeamSubscriptionSerializer(serializers.ModelSerializer[TeamSubscription]):
     class Meta:
         model = TeamSubscription
         fields = "__all__"  # 모든 필드를 포함
