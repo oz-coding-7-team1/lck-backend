a116cd6d27f792743814745d0faaab47cd745466
Jung <jjlee030415@gmail.com>
2025-02-11 21:40:10 +0900

    :pencil2: mypy 실행

--- apps/teams/urls.py
+++ apps/teams/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
+
 from .views import (
-    TeamList,
     TeamDetail,
+    TeamList,
     TeamRank,
-    TeamScheduleList,
     TeamScheduleDetail,
+    TeamScheduleList,
 )
 
 urlpatterns = [
