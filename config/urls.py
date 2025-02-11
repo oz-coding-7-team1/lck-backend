811b8875553df532b27c3e45f6db02e624d23149
BlueLight <dlrkdmf10@gmail.com>
2025-02-11 21:09:45 +0900

    :bulb: players url 수정

--- config/urls.py
+++ config/urls.py
@@ -25,7 +25,7 @@ from drf_spectacular.views import (
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/players", include("apps.players.urls")),
+    path("api/v1/players/", include("apps.players.urls")),
     path("api/v1/subscriptions/", include("apps.subscriptions.urls")),
     path("api/v1/users/", include("apps.users.urls")),
     path("api/v1/schema/", SpectacularAPIView.as_view(), name="schema"),  # API 스키마 엔드포인트
