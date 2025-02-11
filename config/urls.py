2c707ad60f4558a7def4f011f9f37329564a374f
Umdoong <umdoongs@gmail.com>
2025-02-11 15:40:29 +0900

    :sparkles: redoc path 추가

--- config/urls.py
+++ config/urls.py
@@ -34,4 +34,5 @@ urlpatterns = [
     path(
         "api/v1/schema/swgger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"
     ),  # Swagger UI 엔드포인트
+    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
