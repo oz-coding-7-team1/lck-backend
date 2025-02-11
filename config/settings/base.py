c9aed864c802b86fe2cfa4154c037f53c39e5a80
Umdoong <umdoongs@gmail.com>
2025-02-12 01:52:18 +0900

    :sparkles: settings에 STATIC_ROOT 추가

--- config/settings/base.py
+++ config/settings/base.py
@@ -154,6 +154,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
