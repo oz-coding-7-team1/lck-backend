0acee25a7a9e2d32022c14220d9444172eba0998
Umdoong <umdoongs@gmail.com>
2025-02-12 04:28:10 +0900

    :pencil2: prod환경 엑세스 토큰 3시간으로 변경

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,8 +1,6 @@
-import os
-
 from .base import *
 
-DEBUG = False
+DEBUG = True
 
 REFRESH_TOKEN_COOKIE_SECURE = True
 
@@ -10,7 +8,7 @@ ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(",")  # 허용할 ho
 
 SIMPLE_JWT = {
     "AUTH_HEADER_TYPES": ("Bearer",),
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=10),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=3),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
