2d403a9e25319961f6f723ba5a3cc795e9ad2ee5
Jung <jjlee030415@gmail.com>
2025-02-12 00:19:04 +0900

    :bulb: count에다 / 추가

--- apps/subscriptions/urls.py
+++ apps/subscriptions/urls.py
@@ -11,8 +11,8 @@ from .views import (  # TeamSubscriptionDetailView,
 urlpatterns = [
     path("player/<int:player_id>/", PlayerSubscriptionView.as_view(), name="player_subscription"),
     path("player/choeae/", PlayerSubscriptionDetailView.as_view(), name="player_subscription_detail"),
-    path("player/<int:player_id>/count", PlayerSubscriptionCountView.as_view(), name="player_subscription_count"),
+    path("player/<int:player_id>/count/", PlayerSubscriptionCountView.as_view(), name="player_subscription_count"),
     path("team/<int:team_id>/", TeamSubscriptionView.as_view(), name="team_subscription"),
     # path("team/choeae/", TeamSubscriptionDetailView.as_view(), name="team_subscription_detail"),
-    path("team/<int:team_id>/count", TeamSubscriptionCountView.as_view(), name="team_subscription_count"),
+    path("team/<int:team_id>/count/", TeamSubscriptionCountView.as_view(), name="team_subscription_count"),
 ]
