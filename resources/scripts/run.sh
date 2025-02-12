1b324660e3b099b2de9252f5f14d79e9e48ac83b
Umdoong <umdoongs@gmail.com>
2025-02-12 17:44:32 +0900

    :sparkles: run.sh 스크립트 추가

---
+++ resources/scripts/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# 가상 환경 활성화
+source ~/.bashrc
+
+
+export DJANGO_SETTINGS_MODULE=config.settings.dev
+
+# 데이터베이스 마이그레이션
+echo "Applying database migrations..."
+poetry run python manage.py migrate --no-input
+
+# 정적 파일 수집
+echo "Collecting static files..."
+poetry run python manage.py collectstatic --no-input
+
+# Gunicorn 실행
+echo "Starting Gunicorn..."
+exec poetry run gunicorn config.wsgi:application --bind 0.0.0.0:8000
