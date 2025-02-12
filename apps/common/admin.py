adee836080304907eeba1524a0a13d2fb03085d5
Umdoong <umdoongs@gmail.com>
2025-02-12 22:03:01 +0900

    :sparkles: staff도 어드민 페이지

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,3 +1,16 @@
+from typing import Any
+
 from django.contrib import admin
 
-# Register your models here.
+class BaseModelAdmin(admin.ModelAdmin):
+    def has_add_permission(self, request: Any) -> bool:
+        return request.user.is_staff
+
+    def has_change_permission(self, request: Any, obj: object = None) -> bool:
+        return request.user.is_staff
+
+    def has_delete_permission(self, request: Any, obj: object = None) -> bool:
+        return request.user.is_staff
+
+    def has_module_permission(self, request: Any) -> bool:
+        return request.user.is_staff
--- apps/players/admin.py
+++ apps/players/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
+from apps.common.admin import BaseModelAdmin
+
 from .models import Player
 
 
 @admin.register(Player)
-class PlayerAdmin(admin.ModelAdmin):  # type: ignore
+class PlayerAdmin(BaseModelAdmin):
     pass
