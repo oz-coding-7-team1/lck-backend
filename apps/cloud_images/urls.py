b5c1cbebe217b0d89f8d60127a5e10bd6d2013ba
BlueLight <dlrkdmf10@gmail.com>
2025-02-17 14:22:57 +0900

    :sparkles: cloud_images url 설정

--- apps/cloud_images/urls.py
+++ apps/cloud_images/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 
-from .views import PlayerImageListView, TeamImageListView
+from .views import (
+    CloudImageDeleteView,
+    CloudImageListView,
+    CloudImageRetrieveView,
+    CloudImageUploadView,
+)
 
 urlpatterns = [
-    path("player-images/<int:player_id>/", PlayerImageListView.as_view(), name="player_image_list"),
-    path("team-images/<int:team_id>/", TeamImageListView.as_view(), name="team_image_list"),
+    path("upload/", CloudImageUploadView.as_view(), name="upload"),
+    path("list/<str:category>/<str:image_type>/", CloudImageListView.as_view(), name="list"),
+    path("detail/<uuid:image_id>/", CloudImageRetrieveView.as_view(), name="Retrieve"),
+    path("delete/<uuid:image_id>", CloudImageDeleteView.as_view(), name="delete"),
 ]
