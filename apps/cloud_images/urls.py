a635fe3d211f04f95277ee75efc3cf832311885e
BlueLight <dlrkdmf10@gmail.com>
2025-02-18 10:40:18 +0900

    :bulb: image urls.py 수정

--- apps/cloud_images/urls.py
+++ apps/cloud_images/urls.py
@@ -1,15 +1,23 @@
 from django.urls import path
 
 from .views import (
-    CloudImageDeleteView,
-    CloudImageListView,
-    CloudImageRetrieveView,
-    CloudImageUploadView,
+    PlayerGalleryListView,
+    PlayerImageDeleteView,
+    PlayerImageUploadView,
+    TeamGalleryListView,
+    TeamImageDeleteView,
+    TeamImageUploadView,
+    UserImageDeleteView,
+    UserImageUploadView,
 )
 
 urlpatterns = [
-    path("upload/", CloudImageUploadView.as_view(), name="upload"),
-    path("list/<str:category>/<str:image_type>/", CloudImageListView.as_view(), name="list"),
-    path("detail/<uuid:image_id>/", CloudImageRetrieveView.as_view(), name="Retrieve"),
-    path("delete/<uuid:image_id>", CloudImageDeleteView.as_view(), name="delete"),
+    path("user/upload/", UserImageUploadView.as_view(), name="user_upload"),
+    path("user/delete/", UserImageDeleteView.as_view(), name="user_delete"),
+    path("player/upload/", PlayerImageUploadView.as_view(), name="player_upload"),
+    path("player/delete/", PlayerGalleryListView.as_view(), name="player_delete"),
+    path("player/<int:player_id>/gallery/", PlayerGalleryListView.as_view(), name="player_gallery"),
+    path("team/upload/", TeamImageUploadView.as_view(), name="team_upload"),
+    path("team/delete/", TeamImageDeleteView.as_view(), name="team_delete"),
+    path("team/<int:team_id>/gallery/", TeamGalleryListView.as_view(), name="team_gallery"),
 ]
