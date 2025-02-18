c783d70eab56136d662f613e24347f70997febd2
BlueLight <dlrkdmf10@gmail.com>
2025-02-18 10:40:09 +0900

    :bulb: image serializers.py 수정

--- apps/cloud_images/serializers.py
+++ apps/cloud_images/serializers.py
@@ -1,10 +1,24 @@
 from rest_framework import serializers
 
-from .models import CloudImage
+from .models import PlayerImage, TeamImage, UserImage
 
 
-class CloudImageSerializer(serializers.ModelSerializer[CloudImage]):
-
+class UserImageSerializer(serializers.ModelSerializer[UserImage]):
     class Meta:
-        model = CloudImage
-        fields = ["id", "category", "image_type", "image_url", "uploaded_by", "uploaded_at"]
+        model = UserImage
+        fields = ("id", "user", "image_url", "uploaded_at")
+        read_only_fields = ("id", "uploaded_at")
+
+
+class PlayerImageSerializer(serializers.ModelSerializer[PlayerImage]):
+    class Meta:
+        model = PlayerImage
+        fields = ("id", "player", "category", "image_url", "uploaded_by", "uploaded_at")
+        read_only_fields = ("id", "uploaded_by", "uploaded_at")
+
+
+class TeamImageSerializer(serializers.ModelSerializer[TeamImage]):
+    class Meta:
+        model = TeamImage
+        fields = ("id", "team", "category", "image_url", "uploaded_by", "uploaded_at")
+        read_only_fields = ("id", "uploaded_by", "uploaded_at")
