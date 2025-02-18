c01fd6a66f91ea51ad1c64bb0bec933cbe0f0d9e
BlueLight <dlrkdmf10@gmail.com>
2025-02-18 10:39:35 +0900

    :bulb: image admin.py 수정

--- apps/cloud_images/admin.py
+++ apps/cloud_images/admin.py
@@ -1,9 +1,19 @@
 from django.contrib import admin
 
-from apps.cloud_images.models import CloudImage
+from apps.cloud_images.models import PlayerImage, TeamImage, UserImage
 from apps.common.admin import BaseModelAdmin
 
 
-@admin.register(CloudImage)
-class CloudImageAdmin(BaseModelAdmin):
+@admin.register(UserImage)
+class UserImageAdmin(BaseModelAdmin):
+    pass
+
+
+@admin.register(PlayerImage)
+class PlayerImageAdmin(BaseModelAdmin):
+    pass
+
+
+@admin.register(TeamImage)
+class TeamImageAdmin(BaseModelAdmin):
     pass
