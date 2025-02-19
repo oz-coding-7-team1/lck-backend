00505eae50aba5b161710106b8e243b98630e8bd
Umdoong <umdoongs@gmail.com>
2025-02-19 11:11:48 +0900

    :bulb: run black

--- apps/players/admin.py
+++ apps/players/admin.py
@@ -8,24 +8,20 @@ from .models import Player
 
 @admin.register(Player)
 class PlayerAdmin(BaseModelAdmin):
-    list_display = ("id","get_nickname_with_realname", "position", "get_team", "get_tags")
+    list_display = ("id", "get_nickname_with_realname", "position", "get_team", "get_tags")
     list_filter = ("team",)
     search_fields = ("id", "nickname", "realname", "team__name", "tags__name")
     list_display_links = ("id", "get_nickname_with_realname", "get_team")
-    
+
     def get_nickname_with_realname(self, obj):
         return f"{obj.nickname} ({obj.realname})"
-    
+
     def get_team(self, obj):
         if obj.team is None:
             return "-"
-        return format_html(
-            '<a href="{}">{}</a>',
-            f'/admin/teams/team/{obj.team.id}/change/',
-            obj.team
-        )
-    
+        return format_html('<a href="{}">{}</a>', f"/admin/teams/team/{obj.team.id}/change/", obj.team)
+
     def get_tags(self, obj):
         return ", ".join(tag.name for tag in obj.tags.all())
-    
+
     get_tags.short_description = "Tags"
